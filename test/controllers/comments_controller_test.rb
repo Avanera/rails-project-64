ef831db236e642986c1334a91b4ebe63bdeb1a88
Arina Zaitseva <avanero@bk.ru>
2024-08-12 21:34:42 -0400

    Update comments tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -19,6 +19,15 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to post_url(Post.last)
   end
 
+  test 'should not create an empty comment' do
+    assert_no_difference('PostComment.count') do
+      post post_comments_path(post_id: Post.last.id),
+           params: { post_comment: { content: '' } }
+    end
+
+    assert_redirected_to post_url(Post.last)
+  end
+
   test 'should destroy comment' do
     assert_difference('PostComment.count', -1) do
       delete comment_path(PostComment.last)
