e1920c148dea8923fe178a4b5fdd7170098263c9
Arina Zaitseva <avanero@bk.ru>
2024-08-13 00:16:31 -0400

    set locale in params

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -25,5 +24,7 @@ module RailsProject64
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
   end
 end
