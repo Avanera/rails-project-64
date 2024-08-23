9a365ecc2855c678d17e1aef0e8affad48a7b8c4
Arina Zaitseva <avanero@bk.ru>
2024-08-23 19:57:47 -0400

    localise uniqueness error message

--- app/models/post_like.rb
+++ app/models/post_like.rb
@@ -4,5 +4,7 @@ class PostLike < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :user
 
-  validates :user_id, uniqueness: { scope: :post_id, message: 'has already liked this post' }
+  validates :user_id, uniqueness: {
+    scope: :post_id, message: I18n.t('activerecord.errors.models.post_like.already_liked')
+  }
 end
